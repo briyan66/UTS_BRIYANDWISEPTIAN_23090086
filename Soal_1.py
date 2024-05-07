75fe67746b3c63b2821d3a8ee86d4412a1cbf834
briyan66 <145635632+briyan66@users.noreply.github.com>
2024-05-07 16:41:57 +0700

    Add files via upload

---
+++ Soal_1.py
@@ -0,0 +1,14 @@
+def penjumlahan(a,b):
+    return a+b
+
+def pengurangan(a,b):
+    return a-b
+
+def modulus(a,b):
+    return a%b
+
+a=int(input('Masukan bilangan pertama : '))
+b=int(input('Masukan bilangan kedua : '))
+print(penjumlahan(a,b))
+print(pengurangan(a,b))
+print(modulus(a,b))
---
+++ Soal_4.py
@@ -0,0 +1,5 @@
+def pembagian(a,b):
+    return a/b
+a=int(input('masukkan berat badan (Kg): ' ))
+b=int(input('masukkan tinggi badan (Cm): '))
+print('Nilai BMI Anda Adalah',pembagian(a,b))
